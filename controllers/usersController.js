72179b7dbdfc42e95a6e2988ff3b7bc867495c8a
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-04-02 18:33:00 +0530

    nw

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,4 @@
 const pool = require("../config/db");
-
 module.exports = {
   getAllUsersCount: async (req, res) => {
     pool.query(
@@ -14,5 +13,32 @@ module.exports = {
         res.status(200).json(results);
       }
     );
+  },
+  getAllUsersByType: (req, res) => {
+    const user_type = req.query.user_type ? req.query.user_type : null;
+    let sql = "SELECT * FROM users";
+    let countSql = "SELECT COUNT(*) AS count FROM users";
+    let values = [];
+    if (user_type) {
+      sql += " WHERE user_type = ?";
+      countSql += " WHERE user_type = ?";
+      values.push(user_type);
+    }
+    pool.query(countSql, values, (err, countResult) => {
+      if (err) {
+        console.error("Error fetching user count:", err);
+        return res.status(500).json({ error: "Database query failed" });
+      }
+      const userCount = countResult[0].count;
+      pool.query(sql, values, (err, results) => {
+        if (err) {
+          console.error("Error fetching users:", err);
+          return res.status(500).json({ error: "Database query failed" });
+        }
+        res
+          .status(200)
+          .json({ success: true, count: userCount, data: results });
+      });
+    });
   },
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const userController = require("../controllers/usersController");
 
 router.get("/getAllUsersCount", userController.getAllUsersCount);
+router.get("/getAllUsersByType", userController.getAllUsersByType);
 module.exports = router;
