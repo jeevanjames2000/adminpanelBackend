d11b15f2d443f9972fae59acc273ae4011cf6933
jeevan <jeevanjames2000@gmail.com>
2025-04-13 17:54:30 +0530

    sd

--- routes/enquiryRoutes.js
+++ routes/enquiryRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const enquiryController = require("../controllers/enquireController");
 router.get("/getAllEnquiries", enquiryController.getAllEnquiries);
 router.get("/getAllContactSellers", enquiryController.getAllContactSellers);
-router.post("/postIntrest", enquiryController.postEnquiry);
+router.post("/postEnquiry", enquiryController.postEnquiry);
 router.post("/contactSeller", enquiryController.contactSeller);
+router.post("/scheduleVisit", enquiryController.scheduleVisit);
 
 module.exports = router;
