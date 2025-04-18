bade902ccf3982f1d50381d602bc0206ec0a8b7e
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-04-18 09:40:07 +0530

    sd

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const favRoutes = require("./routes/favRoutes");
 const enquiryRoutes = require("./routes/enquiryRoutes");
 const awsRoutes = require("./routes/awsRoutes");
 const app = express();
-
-// Middleware
 const allowedOrigins = [
   "https://testapi.meetowner.in",
   "https://preprod.meetowner.in",
@@ -21,36 +19,22 @@ const allowedOrigins = [
   "http://localhost:5173",
   "*",
 ];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    allowedHeaders:
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    exposedHeaders: ["Content-Disposition"],
-  })
-);
-
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  exposedHeaders: ["Content-Disposition"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-
-// Main Routes
 app.use("/auth/v1", authRoutes);
-
 app.use("/api", Routes);
 app.use("/user", userRoutes);
 app.use("/listings", listingRoutes);
 app.use("/fav", favRoutes);
 app.use("/enquiry", enquiryRoutes);
 app.use("/awsS3", awsRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
